124beb97eee1d7fd05c96d10529882c84e9f0302
BEANANDMENTOS <bean123bbb@gmail.com>
2021-09-05 00:49:30 +0700

    FOLK

--- App.js
+++ App.js
@@ -1,65 +1,101 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { View, StyleSheet, Text, ImageBackground, Button, TouchableWithoutFeedback, Linking } from 'react-native'
 import Sound from "react-native-sound";
-
+import AsyncStorage from "@react-native-async-storage/async-storage"
 
 const App = () => {
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState(null)
   const [isClick, setIsClick] = useState(false);
-  const onPress = () => {
+
+  useEffect(async () => {
+    const count_s = await AsyncStorage.getItem("key");
+    if (count_s) {
+      setCount(parseInt(count_s))
+    } else {
+      await AsyncStorage.setItem('key', "0")
+    }
+  }, [])
+  // let asyncFetch = () => {
+  // if(!AsyncStorage.getItem('key')) {
+  //   try {
+  //     AsyncStorage.setItem('key', 0)
+  //     alert("succ");
+  //   } catch (e) {
+  //     alert("error");
+  //   } 
+  // }
+  //   if (!AsyncStorage.getItem('key')) {
+  //     return 0;
+  //   }
+  //   return AsyncStorage.getItem('key')
+  // }
+
+  // useEffect(async () => {
+  //   await alert("start");
+  //   console.log("eiei")
+  // })
+  // useEffect(() => {
+  //   const asyncFetch = async () => {
+  //     if(!AsyncStorage.getItem("_C")) {
+  //       try {
+  //         await AsyncStorage.setItem("_C", 0)
+  //       } catch (e) {
+  //         console.log(e)
+  //         alert("error");
+  //       }
+  //     }
+  //   }
+  //   asyncFetch();
+  //   setCount(AsyncStorage.getItem("_C"))
+  // }, [])
+
+  const onPress = async () => {
     setCount(count + 1);
+    await AsyncStorage.setItem('key', count.toString())
   }
   Sound.setCategory('Playback');
 
-// const changeIMG = () => {
-//   setIsClick(true);
-//   setTimeout(() => {
-//     onPress();
-//     setIsClick(false);
-//   }, 100)
-// }
-
-const onPressIn = () => {
-  const whoosh = new Sound('sound_pop.mp3', Sound.MAIN_BUNDLE, (error) => {
-    if (error) {
-      return;
-    }
-    whoosh.play((success) => {
-      whoosh.release()
+  const onPressIn = () => {
+    const whoosh = new Sound('sound_pop.mp3', Sound.MAIN_BUNDLE, (error) => {
+      if (error) {
+        return;
+      }
+      whoosh.play((success) => {
+        whoosh.release()
+      });
     });
-  });
-  setIsClick(true);
-}
+    setIsClick(true);
+  }
 
-const onPressOut = () => {
-  onPress();
-  setIsClick(false);
-}
+  const onPressOut = () => {
+    onPress();
+    setIsClick(false);
+  }
 
-return (                 //justifyContent = vertical   alignItems = horizontal
-  <View>
-    <TouchableWithoutFeedback onPressIn={onPressIn} onPressOut={onPressOut} >
-      <View style={styles.container}>
-        <ImageBackground source={isClick ? require('./asset/3.jpg') : require('./asset/2.jpg')} resizeMode="cover" style={styles.image} >
-          <Text style={styles.textHead}>
-            POP MENTOS
-          </Text>
-          <Text style={isClick ? styles.textCount_active : styles.textCount}>
-            {count}
-          </Text>
-          <View style={styles.btn_div}>
-            <Button
-              color="#1B262C"
-              styles={styles.btn}
-              onPress={() => { Linking.openURL('https://www.instagram.com/mr.bean_eiei/') }}
-              title="Press Me"
-            />
-          </View>
-        </ImageBackground>
-      </View>
-    </TouchableWithoutFeedback>
-  </View>
-)
+  return (                 //justifyContent = vertical   alignItems = horizontal
+    <View>
+      <TouchableWithoutFeedback onPressIn={onPressIn} onPressOut={onPressOut} >
+        <View style={styles.container}>
+          <ImageBackground source={isClick ? require('./asset/3.jpg') : require('./asset/2.jpg')} resizeMode="cover" style={styles.image} >
+            <Text style={styles.textHead}>
+              POP MENTOS
+            </Text>
+            <Text style={isClick ? (styles.textCount_active) : (styles.textCount)}>
+              {count}
+            </Text>
+            <View style={styles.btn_div}>
+              <Button
+                color="#1B262C"
+                styles={styles.btn}
+                onPress={() => { Linking.openURL('https://www.instagram.com/mr.bean_eiei/') }}
+                title="Press Me"
+              />
+            </View>
+          </ImageBackground>
+        </View>
+      </TouchableWithoutFeedback>
+    </View>
+  )
 }
 
 const styles = StyleSheet.create({
